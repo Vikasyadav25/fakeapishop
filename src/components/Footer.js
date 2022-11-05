d2733f09300c9c5b82ddddaaacf5d1acae5745fa
Vikasyadav25 <vikassinghyadav6@gmail.com>
2022-11-05 15:55:26 +0530

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../App.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Shopping app 2022 &copy;</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>Shopping App using Fake API</h1>
+      {/* //for demo purpose i use Cart in text form */}
+      <h4>Cart</h4>
+    </div>
+  );
+};
+
+export default Navbar;
